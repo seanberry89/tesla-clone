fd068864d50443807b06546882967249d2c47144
Sean Berry <sean.michael.berry@gmail.com>
2023-03-13 15:57:31 -0500

    added auto scroll

--- script.js
+++ script.js
@@ -6,18 +6,16 @@ const body = document.querySelector('body');
 const fadedBack = document.getElementById('faded-background');
 const downBtn = document.getElementById('down-btn');
 
-const contents = document.querySelectorAll(".showcase-content");
 
-const contentOne = document.getElementById('showcase-content-1');
-const contentTwo = document.getElementById('showcase-content-2');
-const contentThree = document.getElementById('showcase-content-3');
-const contentFour = document.getElementById('showcase-content-4');
-const contentFive = document.getElementById('showcase-content-5');
-const contentSix = document.getElementById('showcase-content-6');
-const contentSeven = document.getElementById('showcase-content-7');
+// Refresh DOM to Page Top
+window.onbeforeunload = () => {
+
+  window.scrollTo(0,0);
+
+};
 
 
-// Event Listeners
+// Event Listener: toggle side menu
 sideMenu.addEventListener('click', (e) => {
   aside.style.right = '0px';
   fadedBack.style.display = 'block';
@@ -42,67 +40,134 @@ fadedBack.addEventListener('click', () => {
 })
 
 
-// DOM Refresh Returns to Page Top
-window.onbeforeunload = () => {
-  window.scrollTo(0,0);
-}
-
-
-// Header Button
+// onClick Event: drop-down button
 downDown = () => {
   window.scrollTo({
     top: 660,
     left: 0,
     behavior: "smooth"
   });
-}
+};
 
+// Event Listener: load drop-down button
 window.addEventListener("load", () => {
+
   downBtn.classList.add("onload");
+
 });
 
+// top: 660 second page
+// top 1325 third page
+// top: 1980 fourth page
+// top: 2640 fifth page
+// top: 3300 sixth page
+// top: 4000 last page
+
+let timer;
+
+// Event Listener: auto scroll for each section
 window.addEventListener("scroll", () => {
-  console.log(window.innerHeight);
-  console.log(contentThree.getBoundingClientRect().top);
+
+  // scroll to the first section
+  if(window.scrollY < 300){
+    if(timer !== null){
+      clearTimeout(timer);
+    };
+
+    timer = setTimeout(() => {
+      window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth"
+      });
+    }, 75);
+
+  // scroll to the second section
+  } else if(window.scrollY > 300 && window.scrollY < 900){
+    if(timer !== null){
+      clearTimeout(timer);
+    };
+
+    timer = setTimeout(() => {
+      window.scrollTo({
+        top: 660,
+        left: 0,
+        behavior: "smooth"
+      });
+    }, 75);
+
+  // scroll to the third section
+  } else if(window.scrollY > 900 && window.scrollY < 1600){
+    if(timer !== null){
+      clearTimeout(timer);
+    };
+
+    timer = setTimeout(() => {
+      window.scrollTo({
+        top: 1320,
+        left: 0,
+        behavior: "smooth"
+      });
+    }, 75);
+
+    // scroll to the fourth section
+  } else if(window.scrollY > 1600 && window.scrollY < 2250){
+    if(timer !== null){
+      clearTimeout(timer);
+    };
+
+    timer = setTimeout(() => {
+      window.scrollTo({
+        top: 1980,
+        left: 0,
+        behavior: "smooth"
+      });
+    }, 75);
+
+    // scroll to the fifth section
+  } else if(window.scrollY > 2250 && window.scrollY < 2900){
+    if(timer !== null){
+      clearTimeout(timer);
+    };
+
+    timer = setTimeout(() => {
+      window.scrollTo({
+        top: 2640,
+        left: 0,
+        behavior: "smooth"
+      });
+    }, 75);
+
+    // scroll to the sixth section
+  } else if(window.scrollY > 2900 && window.scrollY < 3500){
+    if(timer !== null){
+      clearTimeout(timer);
+    };
+
+    timer = setTimeout(() => {
+      window.scrollTo({
+        top: 3300,
+        left: 0,
+        behavior: "smooth"
+      });
+    }, 75);
+
+    // scroll to the last section
+  } else if(window.scrollY > 3500 && window.scrollY < 4400){
+    if(timer !== null){
+      clearTimeout(timer);
+    };
+
+    timer = setTimeout(() => {
+      window.scrollTo({
+        top: 3975,
+        left: 0,
+        behavior: "smooth"
+      });
+    }, 75);
+
+  };
+
+  console.log(window.scrollY);
+
 });
-
-if(contentTwo.getBoundingClientRect().bottom < window.innerHeight){
-  contentOne.classList.add("fade");
-}
-
-
-// {contents.forEach(content => {
-//   window.addEventListener("scroll", () => {
-//     if(content.getBoundingClientRect().bottom < window.innerHeight){
-//       content.classList.add("fade");
-//     } else if (content.getBoundingClientRect().bottom > window.innerHeight) {
-//       content.classList.remove("fade");
-//     }
-//   })
-// })}
-
-// window.addEventListener("scroll", () => {
-//   if((contentTwo.getBoundingClientRect().bottom - 325) < window.innerHeight){
-//     contents.forEach((content) => {
-//       content.classList.add("fade");
-//     })
-//   }
-// });
-
-// window.addEventListener('scroll', () => {
-//   const contentPosition = showcaseHeader.getBoundingClientRect().top;
-//   const screenPosition = window.innerHeight;
-//   if(screenPosition > contentPosition) {
-//     showcaseHeader.classList.add('fade');
-//   }
-// });
-
-// window.addEventListener('scroll', () => {
-//   const contentPosition = showcaseOne.getBoundingClientRect().bottom;
-//   const screenPosition = window.innerHeight;
-//   if(screenPosition > contentPosition) {
-//     content.classList.add('fade');
-//   } else if (screenPosition < contentPosition) {
-//     content.classList.remove('fade');
-//   }
-// })
